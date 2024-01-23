2a005e73946b84bb3b48dcd521903133c0735eaa
Morgan Winkler <winkler.morgan1553@gmail.com>
2024-01-23 14:58:17 -0500

    Change welcome method to take params

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -13,8 +13,8 @@ public class HelloWorldController : Controller
   }
   // 
   // GET: /HelloWorld/Welcome/ 
-  public string Welcome()
+  public string Welcome(string name, int ID = 1)
   {
-    return "This is the Welcome action method...";
+    return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
   }
 }
--- obj/Debug/net8.0/MvcTutorial.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcTutorial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcTutorial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98bf3c5b91b8a58e361dab444688d48b76649de3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72eb689ea1d363f2d1011320d3260a8a021cd71")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcTutorial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcTutorial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
